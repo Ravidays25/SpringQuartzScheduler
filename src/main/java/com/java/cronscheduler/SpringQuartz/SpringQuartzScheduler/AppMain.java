d44087fc044aeb85146e300b85f3137bba4d00ed
Ravi kumar <42302826+Ravidays25@users.noreply.github.com>
2018-10-16 23:11:56 +0530

    changes

--- src/main/java/com/java/cronscheduler/SpringQuartz/SpringQuartzScheduler/AppMain.java
+++ src/main/java/com/java/cronscheduler/SpringQuartz/SpringQuartzScheduler/AppMain.java
@@ -1,13 +1,18 @@
 package com.java.cronscheduler.SpringQuartz.SpringQuartzScheduler;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class AppMain {
 
 	public static void main(String[] args) {
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("quartz-context.xml");
-
+		//AbstractApplicationContext context = new ClassPathXmlApplicationContext("quartz-context.xml");
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("/resoures/quartz-context.xml");
+		
+																		   
 	}
 
 }
--- src/main/java/com/java/cronscheduler/SpringQuartz/SpringQuartzScheduler/MyBean.java
+++ src/main/java/com/java/cronscheduler/SpringQuartz/SpringQuartzScheduler/MyBean.java
@@ -1,13 +1,17 @@
 package com.java.cronscheduler.SpringQuartz.SpringQuartzScheduler;
+import java.io.IOException;
+
 import org.springframework.stereotype.Component;
 @Component("myBean")
 public class MyBean {
 
 	
-	  public void printMessage() {
+	  public void printMessage() throws IOException {
 	        System.out.println("I am called by MethodInvokingJobDetailFactoryBean using SimpleTriggerFactoryBean");
-	        
-	    }
+	       //Runtime.getRuntime().exec("sh ./sample.sh");
+	       
+	       
+	  }
 	
 	
 }
